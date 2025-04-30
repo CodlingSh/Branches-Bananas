ad95fc3f77206ea26f68df1502aee1e948adc1c2
Sheldon Codling <codlinsh@gmail.com>
2025-04-30 04:26:20 -0400

    Added self-masking to the branch sprites

--- main/branch.h
+++ main/branch.h
@@ -17,12 +17,15 @@ const uint8_t PROGMEM medBranch[] = {
 0x00, 0x00, 0x18, 0xff, 0xff, 0xff, 0x00, 
 };
 
-const uint8_t PROGMEM largeBranch[] = {
+const unsigned char PROGMEM largeBranch[] =
+{
+// width, height,
 7, 32,
+// FRAME 00
 0x01, 0x01, 0x03, 0xff, 0xff, 0xff, 0x00, 
 0x00, 0x00, 0x00, 0xf3, 0xff, 0xff, 0x00, 
 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0x03, 
-0x00, 0x00, 0x0c, 0xff, 0xff, 0xff, 0x00, 
+0x00, 0x00, 0x0c, 0xff, 0xff, 0xff, 0x00,
 };
 
 const uint8_t PROGMEM smallBranchR[] = {
@@ -53,22 +56,23 @@ class Branch {
     int _length;
     int _height = 7;
     int _rnd;
-    int _leftOrRight;
+    bool _leftOrRight;
     bool _active = false;
     uint8_t *_currSprite;
+    uint8_t *_currMask;
 
   public:
     int getX() {
       return _x;
-    };
+    }
 
     int getY() {
       return _y;
-    };
+    }
 
     int getLength() {
       return _length;
-    };
+    }
 
     int getHeight() {
       return _height;
@@ -82,31 +86,31 @@ class Branch {
       _x = 140;
       _y = 3;
       _rnd = random(10);
-      _leftOrRight = random(2);
+      _leftOrRight = /*random(2);*/0;
       _active = true;
       
-
       // Decide size of branch
       if (_rnd == 2) {
         _currSprite = (_leftOrRight == 0) ? largeBranch : largeBranchR;
+        _currMask = (_leftOrRight == 0) ? largeBranch_mask : largeBranchR_mask;
         _length = 32;
       }
       else if (_rnd >= 7) {
         _currSprite = (_leftOrRight == 0) ? medBranch : medBranchR;
+        _currMask = (_leftOrRight == 0) ? medBranch_mask : medBranchR_mask;
         _length = 24;
       }
       else {
         _currSprite = (_leftOrRight == 0) ? smallBranch : smallBranchR;
+        _currMask = (_leftOrRight == 0) ? smallBranch_mask : smallBranchR_mask;
         _length = 16;
       }
 
       // Decide what side of screen branch will spawn on
       if (_leftOrRight == 1) {
         _y = 61 - _length;
-      }
-
-      
-    };
+      } 
+    }
 
     void reset() {
       _x = 140;
@@ -125,11 +129,12 @@ class Branch {
           _active = false;
         }
       }
-    };
+    }
 
     void draw() {
-      Sprites::drawOverwrite(_x, _y, _currSprite, 0);
-    };
+      //Sprites::drawExternalMask(_x, _y, _currSprite, _currMask, 0, 0);
+      Sprites::drawSelfMasked(_x, _y, _currSprite, 0);
+    }
 };
 
 #endif
